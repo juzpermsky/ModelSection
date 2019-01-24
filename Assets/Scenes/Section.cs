c5af6cb19634a401ffb1bc9591b51a2d9a772d75
Juz <fcnfhlj1>
2019-01-25 00:35:14 +0500

    Сечение меша плоскостью

--- Assets/Scenes/Section.cs
+++ Assets/Scenes/Section.cs
@@ -60,32 +60,44 @@ public class Section : MonoBehaviour
         */
     }
 
+    private void Update()
+    {
+        Do();
+    }
+
+
     private void Do()
     {
         var triangles = mesh.GetTriangles(0);
         var vertices = mesh.vertices;
-        
+
         var lessIndices = new List<int>();
         var lessPoints = new List<Vector3>();
         var greaterIndices = new List<int>();
         var greaterPoints = new List<Vector3>();
-        for (var i = 0; i < triangles.Length; i += 3)
+        for (var i = 0; i < triangles.Length-3; i += 3)
         {
+            // идем по треугольникам меша
             lessIndices.Clear();
             lessPoints.Clear();
             greaterIndices.Clear();
             greaterPoints.Clear();
             for (var j = 0; j < 3; j++)
             {
-                var index = i + j;
+                // смотрим вершины треугольника
+                // определяем индекс вершины
+                var index = triangles[i + j];
+                // положение вершины в системе координат плоскости сечения 
                 var localPos = transform.InverseTransformPoint(objTransform.TransformPoint(vertices[index]));
                 if (localPos.z < 0)
                 {
+                    // запоминаем вершины, которые ниже плоскости сечения
                     lessIndices.Add(index);
                     lessPoints.Add(localPos);
                 }
                 else
                 {
+                    // запоминаем вершины, которые выше плоскости сечения
                     greaterIndices.Add(index);
                     greaterPoints.Add(localPos);
                 }
@@ -94,7 +106,37 @@ public class Section : MonoBehaviour
             if (lessIndices.Count > 0 && greaterIndices.Count > 0)
             {
                 // Плоскость сечения пересекает этот треугольник
-                //todo: вычисляю линию пересечения
+                var x1 = 0f;
+                var x2 = 0f;
+                var y1 = 0f;
+                var y2 = 0f;
+
+                if (lessIndices.Count == 1)
+                {
+                    // одна вершина снизу, две сверху
+                    x1 = lessPoints[0].x - lessPoints[0].z * (greaterPoints[0].x - lessPoints[0].x) /
+                         (greaterPoints[0].z - lessPoints[0].z);
+                    x2 = lessPoints[0].x - lessPoints[0].z * (greaterPoints[1].x - lessPoints[0].x) /
+                         (greaterPoints[1].z - lessPoints[0].z);
+                    y1 = lessPoints[0].y - lessPoints[0].z * (greaterPoints[0].y - lessPoints[0].y) /
+                         (greaterPoints[0].z - lessPoints[0].z);
+                    y2 = lessPoints[0].y - lessPoints[0].z * (greaterPoints[1].y - lessPoints[0].y) /
+                         (greaterPoints[1].z - lessPoints[0].z);
+                }
+                else
+                {
+                    // одна вершина сверху, две снизу
+                    x1 = greaterPoints[0].x - greaterPoints[0].z * (lessPoints[0].x - greaterPoints[0].x) /
+                         (lessPoints[0].z - greaterPoints[0].z);
+                    x2 = greaterPoints[0].x - greaterPoints[0].z * (lessPoints[1].x - greaterPoints[0].x) /
+                         (lessPoints[1].z - greaterPoints[0].z);
+                    y1 = greaterPoints[0].y - greaterPoints[0].z * (lessPoints[0].y - greaterPoints[0].y) /
+                         (lessPoints[0].z - greaterPoints[0].z);
+                    y2 = greaterPoints[0].y - greaterPoints[0].z * (lessPoints[1].y - greaterPoints[0].y) /
+                         (lessPoints[1].z - greaterPoints[0].z);
+                }
+                // Рисуем линию пересечения
+                Debug.DrawLine(transform.TransformPoint(x1,y1,0),transform.TransformPoint(x2,y2,0),Color.green );
             }
         }
     }
